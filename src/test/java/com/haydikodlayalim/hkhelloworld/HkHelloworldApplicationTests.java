9fce916f063873633ac70344ad2a85f9f0e09a7f
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-29 20:23:20 +0300

    Create project

---
+++ src/test/java/com/haydikodlayalim/hkhelloworld/HkHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.hkhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HkHelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
