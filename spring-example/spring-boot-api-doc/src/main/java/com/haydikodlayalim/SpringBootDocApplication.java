299e5df9db670ae5fcc83c257701153324221737
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-30 14:29:37 +0300

    Ders 6 bitti. Swagger 2 ile API Dökümantasyonu hazırlandı.

---
+++ spring-example/spring-boot-api-doc/src/main/java/com/haydikodlayalim/SpringBootDocApplication.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim;
+
+import io.swagger.annotations.Api;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Api(value = "Benim Pet API dökümantasyonum")
+public class SpringBootDocApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDocApplication.class, args);
+    }
+}
