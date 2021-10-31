8af153baf1e2a5a35dcafb63267acecd673e8ede
Muaz MEMİŞ <muazmemis@gmail.com>
2021-10-31 22:46:30 +0300

    Ders 14 bitti. Rest Client (Rest Template) örneği yapıldı.

--- spring-example/spring-datajpa-postgresql/src/main/java/com/haydikodlayalim/controller/KisiController.java
+++ spring-example/spring-datajpa-postgresql/src/main/java/com/haydikodlayalim/controller/KisiController.java
@@ -11,7 +11,7 @@ import javax.validation.Valid;
 import java.util.List;
 
 @RestController
-@RequestMapping("/kullanici")
+@RequestMapping("/kisi")
 @RequiredArgsConstructor
 public class KisiController {
 
