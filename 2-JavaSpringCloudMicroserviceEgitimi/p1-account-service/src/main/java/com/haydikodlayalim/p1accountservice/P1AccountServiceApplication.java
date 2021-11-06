c0e95c85e598dd0491ead578e683885e2b3215ca
Muaz MEMİŞ <muazmemis@gmail.com>
2021-11-06 20:52:37 +0300

    Ders 2 -> Account Service projesi oluşturuldu.

---
+++ 2-JavaSpringCloudMicroserviceEgitimi/p1-account-service/src/main/java/com/haydikodlayalim/p1accountservice/P1AccountServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.p1accountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P1AccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P1AccountServiceApplication.class, args);
+	}
+
+}
---
+++ 2-JavaSpringCloudMicroserviceEgitimi/p1-account-service/src/test/java/com/haydikodlayalim/p1accountservice/P1AccountServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haydikodlayalim.p1accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P1AccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
