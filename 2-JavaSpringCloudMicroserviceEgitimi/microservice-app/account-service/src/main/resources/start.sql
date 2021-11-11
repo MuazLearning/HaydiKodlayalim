3c94d4e00b07e114535e7b8de65f5643303d7ecc
Muaz MEMİŞ <muazmemis@gmail.com>
2021-11-11 03:15:56 +0300

    Ders 13 bitti. Ticket service için ElasticSearch işlemleri yapıldı.

--- 2-JavaSpringCloudMicroserviceEgitimi/microservice-app/account-service/src/main/resources/start.sql
+++ 2-JavaSpringCloudMicroserviceEgitimi/microservice-app/account-service/src/main/resources/start.sql
@@ -3,12 +3,27 @@ CREATE KEYSPACE springcloud
 
 use springcloud;
 
-CREATE TABLE emp(
-                    emp_id int PRIMARY KEY,
-                    emp_name text,
-                    emp_city text,
-                    emp_sal varint,
-                    emp_phone varint
+CREATE TABLE accounts(
+                    id int PRIMARY KEY,
+                    username text,
+                    name text,
+                    pwd text,
+                    email varint,
+                    birth_date date,
+                    created_at timestamp,
+                    is_active boolean,
 );
 
-select * from emp
+select * from emp;
+
+CREATE TABLE accounts(
+                         id text PRIMARY KEY,
+                         uname text,
+                         name text,
+                         surname text,
+                         pwd text,
+                         email text,
+                         birth_date date,
+                         created_at timestamp,
+                         is_active boolean,
+);
--- 2-JavaSpringCloudMicroserviceEgitimi/microservice-app/ticket-service/src/main/java/com/haydikodlayalim/ticketservice/model/Ticket.java
+++ 2-JavaSpringCloudMicroserviceEgitimi/microservice-app/ticket-service/src/main/java/com/haydikodlayalim/ticketservice/model/Ticket.java
@@ -1,22 +1,32 @@
 package com.haydikodlayalim.ticketservice.model;
 
-import lombok.*;
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.*;
-import java.util.Date;
-
-@Data
+@Entity
+@Table(name = "ticket")
 @EqualsAndHashCode(of = {"id"})
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "ticket")
 public class Ticket extends BaseEntityModel {
 
+    @Id
+    @Getter
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     @Column(name = "id")
-    @Getter
     private String id;
 
     @Getter
@@ -26,7 +36,7 @@ public class Ticket extends BaseEntityModel {
 
     @Getter
     @Setter
-    @Column(name = "notes", length = 4000)
+    @Column(name = "notes", length = 1000)
     private String notes;
 
     @Getter
@@ -41,13 +51,13 @@ public class Ticket extends BaseEntityModel {
 
     @Getter
     @Setter
-    @Enumerated(EnumType.ORDINAL) // Default
+    @Enumerated(EnumType.ORDINAL)
     @Column(name = "priority_type")
     private PriorityType priorityType;
 
     @Getter
     @Setter
-    @Enumerated
+    @Enumerated(EnumType.ORDINAL)
     @Column(name = "ticket_status")
     private TicketStatus ticketStatus;
 
