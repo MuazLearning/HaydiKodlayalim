b96b07af3f1dcff31a7b18a38de5a85af8ec8296
Muaz MEMİŞ <muazmemis@gmail.com>
2022-03-30 21:33:38 +0300

    Ders 1 bitti. JUnit test.

--- 3-JavaUnitTestEgitimi(BirimTestGelistirme)/junit-test/src/test/java/com/haydikodlayalim/service/impl/KisiServiceImplTest.java
+++ 3-JavaUnitTestEgitimi(BirimTestGelistirme)/junit-test/src/test/java/com/haydikodlayalim/service/impl/KisiServiceImplTest.java
@@ -1,22 +1,47 @@
 package com.haydikodlayalim.service.impl;
 
+import com.haydikodlayalim.dto.KisiDto;
+import com.haydikodlayalim.entity.Kisi;
+import com.haydikodlayalim.repo.AdresRepository;
+import com.haydikodlayalim.repo.KisiRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class KisiServiceImplTest {
 
-    @Test
-    public void save() {
-    }
+    @InjectMocks
+    private KisiServiceImpl kisiService;
+
+    @Mock
+    private KisiRepository kisiRepository;
+
+    @Mock
+    private AdresRepository adresRepository;
 
     @Test
-    public void delete() {
-    }
+    public void testSave() {
+        KisiDto kisiDto = new KisiDto();
+        kisiDto.setAdi("Test-Name");
+        kisiDto.setSoyadi("Test-LastName");
+        kisiDto.setAdresler(Arrays.asList("Test-Address-1"));
 
-    @Test
-    public void getAll() {
-    }
+        Kisi kisiMock = mock(Kisi.class);
+        when(kisiMock.getId()).thenReturn(1L);
+        when(kisiRepository.save(any(Kisi.class))).thenReturn(kisiMock);
+        KisiDto result = kisiService.save(kisiDto);
 
-    @Test
-    public void testGetAll() {
+        assertEquals(result.getAdi(), kisiDto.getAdi());
+        assertEquals(result.getId(), 1L);
+        
     }
 }
