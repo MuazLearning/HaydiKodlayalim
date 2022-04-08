afbac09fba3c1850f9bac83237770f48f8995e6b
Muaz MEMİŞ <muazmemis@gmail.com>
2022-04-08 16:10:46 +0300

    Ders 3 - Product Model

---
+++ 7-SpringBoot2ReactiveAndVue3/hk-shopping-app/src/main/java/com/haydikodlayalim/shoppingapp/product/domain/Product.java
@@ -0,0 +1,8 @@
+package com.haydikodlayalim.shoppingapp.product.domain;
+
+public class Product {
+    private String id;
+    private String name;
+    private String productCode;
+    private Boolean active;
+}
---
+++ 7-SpringBoot2ReactiveAndVue3/hk-shopping-app/src/main/java/com/haydikodlayalim/shoppingapp/product/model/ProductResponse.java
@@ -0,0 +1,6 @@
+package com.haydikodlayalim.shoppingapp.product.model;
+
+public class ProductResponse {
+    private String id;
+
+}
