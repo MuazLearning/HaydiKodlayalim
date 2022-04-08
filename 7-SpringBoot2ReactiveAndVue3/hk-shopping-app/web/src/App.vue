bec8757534dfa2b5bf3bc5e86982797350388fae
Muaz MEMİŞ <muazmemis@gmail.com>
2022-04-08 18:01:05 +0300

    Ders 4 - Front End - Product Cart

--- 7-SpringBoot2ReactiveAndVue3/hk-shopping-app/web/src/App.vue
+++ 7-SpringBoot2ReactiveAndVue3/hk-shopping-app/web/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ProductVue/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductVue from './components/Product.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductVue
   }
 }
 </script>
@@ -21,7 +20,7 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  background-color: #f6f6f6;
   color: #2c3e50;
   margin-top: 60px;
 }
