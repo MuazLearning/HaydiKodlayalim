fed0190c689ee7c0cc6949997883db83429e5a61
Muaz MEMİŞ <muazmemis@gmail.com>
2022-04-17 13:51:24 +0300

    Ders 15 - Product Detail components data bindings

--- 7-SpringBoot2ReactiveAndVue3/hk-shopping-app/src/main/java/com/haydikodlayalim/shoppingapp/product/api/ProductApi.java
+++ 7-SpringBoot2ReactiveAndVue3/hk-shopping-app/src/main/java/com/haydikodlayalim/shoppingapp/product/api/ProductApi.java
@@ -1,13 +1,12 @@
 package com.haydikodlayalim.shoppingapp.product.api;
 
+import com.haydikodlayalim.shoppingapp.product.model.product.ProductDetailResponse;
 import com.haydikodlayalim.shoppingapp.product.model.product.ProductResponse;
 import com.haydikodlayalim.shoppingapp.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/products")
@@ -20,6 +19,11 @@ public class ProductApi {
     @GetMapping
     public Flux<ProductResponse> getAllProducts() {
         return productService.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Mono<ProductDetailResponse> getProductDetail(@PathVariable("id") String id) {
+        return productService.getProductDetail(id);
     }
 
 }
