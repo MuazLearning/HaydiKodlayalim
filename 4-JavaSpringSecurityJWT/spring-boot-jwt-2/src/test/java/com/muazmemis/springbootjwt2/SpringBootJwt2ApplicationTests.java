04a7d815abcee9b4d51d2f959eed17e6180b8e93
Muaz Memiş <muazmemis@gmail.com>
2023-03-22 22:52:54 +0300

    jwt 2 init

---
+++ 4-JavaSpringSecurityJWT/spring-boot-jwt-2/src/test/java/com/muazmemis/springbootjwt2/SpringBootJwt2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muazmemis.springbootjwt2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwt2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
